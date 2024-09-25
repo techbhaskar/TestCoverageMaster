7314425ad6c969dc3008b92893276e70bbdf2984
Replit User <replit@example.com>
2024-09-25 04:46:47 +0000

    Update version to 1.0.1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from visualization import display_coverage, display_test_quality, display_functi
 from utils import process_upload
 
 # Add version number
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 def get_file_extension(project_type):
     if project_type in ["JavaScript", "React"]:
